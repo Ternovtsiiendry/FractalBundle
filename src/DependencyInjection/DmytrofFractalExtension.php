d174826a807ac8296ba4bdab4e097d34f8f3099b
Dmytro Feshchenko <dmytro.feshchenko@gmail.com>
2019-11-18 16:15:02 +0200

    Structure refactoring

--- src/DependencyInjection/DmytrofFractalExtension.php
+++ src/DependencyInjection/DmytrofFractalExtension.php
@@ -22,7 +22,7 @@ class DmytrofFractalExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
         $container->registerForAutoconfiguration(AbstractTransformer::class)
